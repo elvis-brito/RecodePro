1f3de49b765abc55466290bf14969ad80195ef51
Elvis Brito <elvisbritodossantos61@gmail.com>
2021-02-12 23:16:57 -0300

    next

--- back-end/app.js
+++
@@ -1,5 +0,0 @@
-var app = require('./config/server');
-
-app.listen(5000, () => {
-    console.log("Servidor ativo!");
-});
--- back-end/bd/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql')
-const connect = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'mydb'
-    })
-
-module.exports = connect
